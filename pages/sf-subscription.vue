c79adc9e6e29209cc04a3bc0b365fc0735e7d98b
ybw0014 <i@guizhanss.com>
2023-07-09 18:43:23 -0400

    feat: download button

--- pages/sf-subscription.vue
+++ pages/sf-subscription.vue
@@ -86,6 +86,10 @@ async function checkOrder() {
   devDownloadLink.value = downloadLink.value;
   queryBtn.value.disabled = false;
 }
+
+function devDownload() {
+  window.open(devDownloadLink.value, "_blank", "noopener noreferrer");
+}
 </script>
 
 <template>
@@ -152,6 +156,9 @@ async function checkOrder() {
             <button ref="queryBtn" type="button" class="button primary" @click="checkOrder">
               {{ t('pages.sfSubscription.devCheck.query') }}
             </button>
+            <button v-if="devDownloadLink" type="button" class="button primary" @click="devDownload">
+                {{ t('pages.sfSubscription.devCheck.download') }}
+              </button>
           </div>
         </div>
         <div class="flex gap-2 flex-wrap mt-4">
