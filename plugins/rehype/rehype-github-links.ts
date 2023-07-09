ceebe09a4144d89bd56ed8836921dd102bb1e01d
ybw0014 <i@guizhanss.com>
2023-07-08 23:25:34 -0400

    feat(readme): parse relative links

---
+++ plugins/rehype/rehype-github-links.ts
@@ -0,0 +1,41 @@
+import { Plugin } from "unified";
+import { Root } from "hast";
+import { visit } from "unist-util-visit";
+import _ from "lodash";
+
+export interface Options {
+  repo: string,
+  branch: string,
+}
+
+interface LinkNode {
+  type: "element"
+  tagName: string
+  properties: {
+    [attr: string]: string
+  }
+}
+
+const defaultOptions: Partial<Options> = {
+};
+
+const plugin: Plugin<[Options?], Root> = (options?: Partial<Options>) => {
+  const finalOptions = _.defaults(options, defaultOptions);
+  return (tree: any) => {
+    visit(tree, { type: "element", tagName: "a" }, (node) => {
+      const linkNode = node as any as LinkNode;
+      if (!linkNode.properties) {
+        return;
+      }
+      const href = linkNode.properties.href;
+      if (!href || typeof href !== "string" || href.startsWith("/external")) {
+        return;
+      }
+      const link = new URL((finalOptions.repo || "/") + `/tree/${finalOptions.branch}/${href}`, "https://github.com/").toString();
+      linkNode.properties.href = "/external?link=" + encodeURIComponent(link);
+      linkNode.properties.target = "_blank";
+    });
+  };
+};
+
+export default plugin;
