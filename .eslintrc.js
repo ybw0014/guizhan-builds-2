224c1f9ac7e1ff2bf2165fbc840b3ff2084ab247
ybw0014 <i@guizhanss.com>
2023-07-22 11:17:56 -0400

    chore: cleanup after update

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     "vue/multi-word-component-names": ["off"],
     "vue/no-multiple-template-root": ["off"],
     "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/no-unused-vars": ["warn"],
     "import/no-unresolved": ["off"]
   }
 };
