d6e4cdf39dc0c5cf67c72a49a799420d51c1c282
ybw0014 <i@guizhanss.com>
2023-08-02 21:22:48 -0400

    feat(logo): implement logo

--- components/project/Logo.vue
+++ components/project/Logo.vue
@@ -2,11 +2,19 @@
 import { Project } from "guizhan-builds-2-data";
 import LazyLoadImage from "~/components/LazyLoadImage.vue";
 
-defineProps<{
+const props = defineProps<{
   project: Project;
 }>();
+
+const defaultImage = "/images/default_plugin.webp";
+const projectIcon = computed(() => {
+  if (props.project.displayOptions?.logo) {
+    return `https://cdn.jsdelivr.net/gh/SlimefunGuguProject/Slimefun-Icons/images/${props.project.displayOptions.logo}`;
+  }
+  return defaultImage;
+});
 </script>
 
 <template>
-  <LazyLoadImage source="/images/default_plugin.webp" broken-image="/images/default_plugin.webp" class="rounded-md" />
+  <LazyLoadImage :source="projectIcon" :broken-image="defaultImage" class="rounded-md" />
 </template>
