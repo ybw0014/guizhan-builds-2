1960da0c59b8bfef3435ee627bd6a1efb4ea36a2
ybw0014 <i@guizhanss.com>
2023-08-08 22:31:41 -0400

    fix(content): fix query path issue

--- components/MarkdownContent.vue
+++ components/MarkdownContent.vue
@@ -3,13 +3,16 @@ const props = defineProps<{
   page: string;
 }>();
 const { locale } = useI18n();
-const { data } = await useAsyncData(props.page, () => queryContent(`/${locale.value}/${props.page}`).findOne());
+const path = `/${locale.value.toLowerCase()}/${props.page}`;
 </script>
 
 <template>
-  <ContentRenderer tag="article" :value="data" class="markdown">
+  <ContentDoc :path="path" tag="article" class="markdown">
     <template #empty>
       Loading...
     </template>
-  </ContentRenderer>
+    <template #not-found>
+      Error
+    </template>
+  </ContentDoc>
 </template>
