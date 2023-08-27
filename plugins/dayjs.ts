7e80fd62948581912179855a31d701977ede9e9f
ybw0014 <i@guizhanss.com>
2023-08-27 16:16:59 -0400

    feat: add vitest

--- plugins/dayjs.ts
+++ plugins/dayjs.ts
@@ -9,15 +9,19 @@ export default defineNuxtPlugin(() => {
   return {
     provide: {
       dayjs: (cfg?: ConfigType) => {
-        switch (useI18n().locale.value) {
-          case "zh-CN":
-            dayjs.locale("zh-cn");
-            break;
-          case "zh-TW":
-            dayjs.locale("zh-tw");
-            break;
-          default:
-            dayjs.locale("en");
+        try {
+          switch (useI18n().locale.value) {
+            case "zh-CN":
+              dayjs.locale("zh-cn");
+              break;
+            case "zh-TW":
+              dayjs.locale("zh-tw");
+              break;
+            default:
+              dayjs.locale("en");
+          }
+        } catch (e) {
+          dayjs.locale("en");
         }
         return dayjs(cfg);
       },
