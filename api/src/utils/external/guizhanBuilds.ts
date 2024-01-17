be294b29e6da7e39c726fc369fcef4a7d1575838
ybw0014 <i@guizhanss.com>
2024-01-17 01:50:41 -0500

    feat(api): allow alias

--- api/src/utils/external/guizhanBuilds.ts
+++ api/src/utils/external/guizhanBuilds.ts
@@ -1,5 +1,5 @@
 import { requestJson } from '~/utils/request'
-import { BuildsInfo, Projects, Project, useParseProjects, BuildInfo } from 'guizhan-builds-2-data'
+import { type BuildsInfo, type Projects, type Project, type BuildInfo, useParseProjects } from 'guizhan-builds-2-data';
 
 export async function fetchProjects(): Promise<Project[]> {
   const rawProjects = await requestJson<Projects>(
@@ -8,14 +8,17 @@ export async function fetchProjects(): Promise<Project[]> {
   return useParseProjects(rawProjects)
 }
 
-export async function fetchProject(author: string, repo: string, branch?: string): Promise<Project | undefined> {
+export async function fetchProject(author: string, repo: string, branch: string): Promise<Project | undefined> {
   const projects = await fetchProjects()
-  return projects.find(project => 
-    project.author && project.repository && 
-      project.author.toLocaleLowerCase() === author.toLocaleLowerCase() && 
-      project.repository.toLocaleLowerCase() === repo.toLocaleLowerCase() 
-      ? !branch || project.branch === branch : false
-  )
+  return projects.find(project => {
+    if (project.author === author && project.repository === repo && project.branch === branch) {
+      return true
+    }
+    if (Array.isArray(project.alias)) {
+      return project.alias.find(alias => `${author}/${repo}:${branch}` === alias)
+    }
+    return false
+  })
 }
 
 export async function fetchBuilds(r2: R2Bucket, project: Project): Promise<BuildsInfo | null> {
