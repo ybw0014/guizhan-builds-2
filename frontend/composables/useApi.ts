f1ecbfa36d948a51cd63a4c5510fb22d74be2212
ybw0014 <i@guizhanss.com>
2024-01-18 16:04:58 -0500

    chore(frontend): bump nuxt to 3.9.3

--- frontend/composables/useApi.ts
+++ frontend/composables/useApi.ts
@@ -8,15 +8,15 @@ export async function useLocalApi<T>(path: string) {
     host = window.location.host;
     protocol = window.location.protocol;
   }
-  return useAsyncData<T>(path, () => $fetch(`${protocol}//${host}${path}`));
+  return await useAsyncData<T>(path, () => $fetch(`${protocol}//${host}${path}`));
 }
 
 export async function useContentApi<T>(path: string) {
-  return useAsyncData<T>(path, () => queryContent<T>(path).findOne());
+  return await useAsyncData<T>(path, () => queryContent<T>(path).findOne());
 }
 
 export async function useExternalApi<T>(path: string) {
-  return useAsyncData<T>(path, () => $fetch(path));
+  return await useAsyncData<T>(path, () => $fetch(path));
 }
 
 export async function useR2Asset<T>(path: string) {
